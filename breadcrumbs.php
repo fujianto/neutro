6cf6a7d8ab7bbd06becce3144ab5dfd9d8f795ed
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-11-01 23:30:27 +0700

    Initial release

---
+++ breadcrumbs.php
@@ -0,0 +1,14 @@
+<?php if(!is_home() && !is_front_page() ){
+
+if ( current_theme_supports( 'breadcrumb-trail' ) ) {
+	breadcrumb_trail(
+		array( 
+			'container' => 'nav', 
+			'separator' => '&rarr;', 
+			'labels'    => array( 
+					'browse' => __( 'Browse: ', 'neutro' ) 
+				) 
+		) 
+	);
+
+}} ?>
