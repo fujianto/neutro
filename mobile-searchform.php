8d634af1b3c0b0e2df5d48702e17b98b9dfaead7
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-12-18 15:07:27 +0700

    Better semantic

--- mobile-searchform.php
+++ mobile-searchform.php
@@ -12,7 +12,7 @@
 
 <div class="container">
   <div class="row">
-    <div class="span12"> 
+    <div class="span12" id="mobile-search-wrapper"> 
       <div class="mobile-search-widget">
         <form class="navbar-form" id="mobile-top-search" action="<?php echo trailingslashit( home_url() ); ?>">
           <input id="mobile-search" type="search"  name="s" value="<?php if ( is_search() ) echo esc_attr( get_search_query() ); else esc_attr_e( 'Search this site...', 'neutro' ); ?>">          
