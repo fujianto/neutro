9239b528d3efb4f72c55397d4f0aece2ef09708e
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2015-01-18 20:50:09 +0700

    Organize content meta

---
+++ template/comment-meta.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Comment meta Template
+ *
+ * Display meta information on Comment list
+ *
+ * @package Neutro
+ * @subpackage Template
+ * @since 1.1.3
+ */
+?>
+
+<div class="comment-meta">
+	<?php echo hybrid_comment_author_shortcode(''); ?>
+	<div class="comment-meta-wrapper"><span class="genericon genericon-time"></span>
+		<?php echo hybrid_comment_published_shortcode(array('after' => ' | ', 'human_time' => __( '%s ago', 'neutro' ))); ?>
+		<?php echo hybrid_comment_permalink_shortcode(array('after' => ' | ')); ?>
+		<?php echo hybrid_comment_edit_link_shortcode(array('after' => '  ')); ?>
+	</div>	
+</div>
---
+++ template/long-content-meta.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Long Content Template
+ *
+ * Display meta information on Content area on Index / front page
+ *
+ * @package Neutro
+ * @subpackage Template
+ * @since 1.1.3
+ */
+?>
+
+<p class="article-meta-extra"> 
+	<?php echo hybrid_entry_author_shortcode(array( 'before' => '', 'after' => ' | ' )); ?>  
+	<a href="<?php echo get_permalink(); ?>"><?php echo hybrid_entry_published_shortcode(''); ?></a> | 
+	<?php echo hybrid_entry_terms_shortcode(array('taxonomy' => 'category', 'separator' => ', ', 'before' => 'In ', 'after' => ' | ' )); ?>
+	<?php echo hybrid_entry_comments_link_shortcode(array( 'after' => ' | ')); ?>
+	<?php echo hybrid_entry_edit_link_shortcode(array( 'after' => ' ')); ?>
+</p>	
